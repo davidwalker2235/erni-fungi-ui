69c59820c372a7e1d0b5e7c0dee1c4221b70e1de
david <davidwalker2235@gmail.com>
2025-02-17 09:18:30 +0100

    first upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Alert } from 'flowbite-svelte';
+    import { Button } from 'flowbite-svelte';
+  </script>
+  
+  <div class="p-8">
+    <Button>Default</Button>
+    <Button color="alternative">Alternative</Button>
+    <Button color="dark">Dark</Button>
+    <Button color="light">Light</Button>
+    <Button color="blue">Blue</Button>
+    <Button color="green">Green</Button>
+    <Button color="red">Red</Button>
+    <Button color="yellow">Yellow</Button>
+    <Button color="purple">Purple</Button>
+  </div>
