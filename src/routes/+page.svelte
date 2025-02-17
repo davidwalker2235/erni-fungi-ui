699717542aa3d6269f56b8b9139418cac47a32f6
david <davidwalker2235@gmail.com>
2025-02-17 11:38:17 +0100

    add login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script>
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
     import { Button } from 'flowbite-svelte';
+    import { isLoggedIn } from '$lib/localStorage.js';
+
+    onMount(() => {
+        if (isLoggedIn()) {
+            goto('./main');
+        } else {
+            goto('./login');
+        }
+    });
 </script>
   
 <div class="p-8">
     <Button>Default</Button>
-    <Button color="alternative">Alternative</Button>
-    <Button color="dark">Dark</Button>
-    <Button color="light">Light</Button>
-    <Button color="blue">Blue</Button>
-    <Button color="green">Green</Button>
-    <Button color="red">Red</Button>
-    <Button color="yellow">Yellow</Button>
-    <Button color="purple">Purple</Button>
 </div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { Card, Input, Label, Button } from 'flowbite-svelte';
+    import { setItem } from '$lib/localStorage.js';
+    import { goto } from '$app/navigation';
+
+    let email = '';
+    let password = '';
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (email === 'erni' && password === 'erni') {
+            setItem('loggedIn', 'true');
+            goto('/main');
+        } else {
+            alert('incorrect user or pass');
+        }
+    }
+</script>
+
+<div class="flex items-center justify-center min-h-screen">
+    <Card>
+        <form on:submit={handleSubmit}>
+            <div class="grid gap-12 mb-12 md:grid-cols-1">
+                <div class="mb-6">
+                    <Label for="user" class="mb-2">Email address</Label>
+                    <Input type="text" id="email" bind:value={email} placeholder="Your name" required />
+                </div>
+                <div class="mb-6">
+                    <Label for="password" class="mb-2">Password</Label>
+                    <Input type="password" id="password" bind:value={password} placeholder="•••••••••" required />
+                </div>
+                <Button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white">Submit</Button>
+            </div>
+        </form>
+    </Card>
+</div>
