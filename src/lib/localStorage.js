f0e1a5db95f30d052cb6790a7e66b55a393f3e2a
david <davidwalker2235@gmail.com>
2025-02-17 09:50:20 +0100

    add localstorage module

---
+++ src/lib/localStorage.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} key
+ * @param {string} value
+ */
+export function setItem(key, value) {
+    localStorage.setItem(key, value);
+}
+
+/**
+ * @param {string} key
+ * @returns {string | null}
+ */
+export function getItem(key) {
+    return localStorage.getItem(key);
+}
+
+/**
+ * @param {string} key
+ */
+export function removeItem(key) {
+    localStorage.removeItem(key);
+}
+
+export function clear() {
+    localStorage.clear();
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script>
-    import { Alert } from 'flowbite-svelte';
     import { Button } from 'flowbite-svelte';
-  </script>
+</script>
   
-  <div class="p-8">
+<div class="p-8">
     <Button>Default</Button>
     <Button color="alternative">Alternative</Button>
     <Button color="dark">Dark</Button>
@@ -13,4 +12,4 @@
     <Button color="red">Red</Button>
     <Button color="yellow">Yellow</Button>
     <Button color="purple">Purple</Button>
-  </div>
+</div>
